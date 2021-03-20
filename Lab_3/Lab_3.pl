d0c7a52bc6531ebf530d67fcb6d8f60e49e7f6a8
Neitrino909 <62421221+Neitrino909@users.noreply.github.com>
2021-03-20 20:30:56 +0300

    Update Lab_3.pl

--- Lab_3/Lab_3.pl
+++ Lab_3/Lab_3.pl
@@ -12,4 +12,5 @@ fact_d(X,Y,X,Y):-!.
 fact_d(X,Y,X1,Y1):-X2 is X1+1,Y2 is Y1*X2,fact_d(X,Y,X2,Y2).
 fact_down(X,Y):-fact_d(X,Y,1,1).
 
-
+fib_up(2,1):-!.
+fib_up(X,Y):-X1 is X-1, fibUp(X1,Y1), Y is Y+Y1.
