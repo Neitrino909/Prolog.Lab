0e00af9beaf5eedf4deab85d40eace9f1578be6c
Neitrino909 <62421221+Neitrino909@users.noreply.github.com>
2021-03-21 19:14:17 +0300

    Update Lab_4.pl

--- Lab_4/Lab_4.pl
+++ Lab_4/Lab_4.pl
@@ -1,2 +1,7 @@
+% Exercise 1
 writeList([]):-!.
 writeList([H|T]):-write(H),write(" "),writeList(T).
+
+r_list(N,N,L,L):-!.
+r_list(N,I,VSL,L):-I1 is I+1, read(X),append(VSL,[X],VSL1),r_list(N,I1,VSL1,L).
+read_list(N,L):-r_list(N,0,[],L).
