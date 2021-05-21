4877cc9fa6b2d1f1f547862d50737788f6db0ada
Neitrino909 <62421221+Neitrino909@users.noreply.github.com>
2021-05-21 23:31:21 +0300

    Update Laba_9.pl

--- Lab_9/Laba_9.pl
+++ Lab_9/Laba_9.pl
@@ -57,3 +57,30 @@ sochet_p([],0,_):-!.
 sochet_p([H|Sub_set],K,[H|Set]):- K1 is K-1, sochet_p(Sub_set,K1,[H|Set]).
 sochet_p(Sub_set,K,[_|Set]):- sochet_p(Sub_set,K,Set).
 pr_sochet_p:- read_str(A,_), read(K), sochet_p(B,K,A), write_str(B), nl, fail.
+
+% 2
+upr2:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), pr2, told.
+pr2:- Positions = [0,1,2,3,4], Itog_slovo = [_,_,_,_,_], 
+	sochet(A_positions,2,Positions), put_letter(Itog_slovo,A_positions,0,a),
+	in_list([b,c,d,e,f],El1), put_free(Itog_slovo,El1),
+	in_list([b,c,d,e,f],El2), put_free(Itog_slovo,El2),
+	in_list([b,c,d,e,f],El3), put_free(Itog_slovo,El3),
+	write_list(Itog_slovo), nl, fail.
+
+% put_letter(Word,Letter_positions,Letter).
+% Ставим букву Letter на все позиции Letter_positions в списке Word.
+put_letter(_,[],_,_):-!.
+put_letter([H|T1],[I|T2],I,H):- I1 is I+1, put_letter(T1,T2,I1,H),!.
+put_letter([_|T],Letter_positions,I,L):- I1 is I+1, put_letter(T,Letter_positions,I1,L).
+
+put_free([H|_],L):- var(H), H = L,!.
+put_free([_|T],L):- put_free(T,L),!.
+
+% 3
+upr3:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), pr3, told.
+pr3:- Positions = [0,1,2,3,4], Itog_slovo = [_,_,_,_,_], 
+	sochet(A_positions,2,Positions), put_letter(Itog_slovo,A_positions,0,a),
+	in_list_exlude([b,c,d,e,f],El1,A1), put_free(Itog_slovo,El1),
+	in_list_exlude(A1,El2,A2), put_free(Itog_slovo,El2),
+	in_list(A2,El3), put_free(Itog_slovo,El3),
+	write_list(Itog_slovo), nl, fail.
