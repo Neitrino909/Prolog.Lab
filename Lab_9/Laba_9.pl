9667fffbc1b0971a90e29d3fd97a19d3f239b7d4
Neitrino909 <62421221+Neitrino909@users.noreply.github.com>
2021-05-21 22:03:12 +0300

    Update Laba_9.pl

--- Lab_9/Laba_9.pl
+++ Lab_9/Laba_9.pl
@@ -17,3 +17,20 @@ build_all_razm_p:- read_str(A,_), read(K), b_a_rp(A,K,[]).
 
 b_a_rp(_,0,Perm1):- write_str(Perm1), nl, !, fail.
 b_a_rp(A,N,Perm):- in_list(A,El), N1 is N-1, b_a_rp(A,N1,[El|Perm]).
+
+% 1_2
+upr1_2:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), build_all_perm, told.
+build_all_perm:- read_str(A,_), b_a_p(A,[]).
+
+in_list_exlude([El|T],El,T).
+in_list_exlude([H|T],El,[H|Tail]):- in_list_exlude(T,El,Tail).
+
+b_a_p([],Perm1):- write_str(Perm1), nl, !, fail.
+b_a_p(A,Perm):- in_list_exlude(A,El,A1), b_a_p(A1,[El|Perm]).
+
+% 1_3
+upr1_3:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), build_all_razm, told.
+build_all_razm:- read_str(A,_), read(K), b_a_r(A,K,[]).
+		
+b_a_r(_,0,Perm1):- write_str(Perm1), nl, !, fail.
+b_a_r(A,N,Perm):- in_list_exlude(A,El,A1), N1 is N-1, b_a_r(A1,N1,[El|Perm]).
