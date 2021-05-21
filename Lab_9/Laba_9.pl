867583abf81bc0826df8efe7f8d42ba19c29ad76
Neitrino909 <62421221+Neitrino909@users.noreply.github.com>
2021-05-21 22:50:01 +0300

    Update Laba_9.pl

--- Lab_9/Laba_9.pl
+++ Lab_9/Laba_9.pl
@@ -34,3 +34,26 @@ build_all_razm:- read_str(A,_), read(K), b_a_r(A,K,[]).
 		
 b_a_r(_,0,Perm1):- write_str(Perm1), nl, !, fail.
 b_a_r(A,N,Perm):- in_list_exlude(A,El,A1), N1 is N-1, b_a_r(A1,N1,[El|Perm]).
+
+% 1_4
+upr1_4:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), pr_subset, told.
+pr_subset:- read_str(A,_), sub_set(B,A), write_str(B), nl, fail.
+
+sub_set([],[]).
+sub_set([H|Sub_set],[H|Set]):- sub_set(Sub_set,Set).
+sub_set(Sub_set,[_|Set]):- sub_set(Sub_set,Set).
+
+% 1_5
+upr1_5:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), pr_sochet, told.
+pr_sochet:- read_str(A,_), read(K), sochet(B,K,A), write_str(B), nl, fail.
+sochet([],0,_):-!.
+sochet([H|Sub_set],K,[H|Set]):- K1 is K-1, sochet(Sub_set,K1,Set).
+sochet(Sub_set,K,[_|Set]):- sochet(Sub_set,K,Set).
+
+% 1_6
+upr1_6:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), pr_sochet_p, told.
+
+sochet_p([],0,_):-!.
+sochet_p([H|Sub_set],K,[H|Set]):- K1 is K-1, sochet_p(Sub_set,K1,[H|Set]).
+sochet_p(Sub_set,K,[_|Set]):- sochet_p(Sub_set,K,Set).
+pr_sochet_p:- read_str(A,_), read(K), sochet_p(B,K,A), write_str(B), nl, fail.
