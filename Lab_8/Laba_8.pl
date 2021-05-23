bb79768de8e7874cc8d741acc00fb5b72920ba93
Neitrino909 <62421221+Neitrino909@users.noreply.github.com>
2021-05-23 16:04:10 +0300

    Update Laba_8.pl

--- Lab_8/Laba_8.pl
+++ Lab_8/Laba_8.pl
@@ -1,19 +1,120 @@
-in_list([El|_],El).
-in_list([_|T],El):-in_list(T,El).
-
-read_str(A,N):- get0(X), r_str(X,A,[],N,0).
-r_str(10,A,A,N,N):-!.
-r_str(X,A,B,N,K):- K1 is K+1, append(B,[X],B1), get0(X1), r_str(X1,A,B1,N,K1).
-
-write_list([]):-!.
-write_list([Head|Tail]):- write(Head), write_list(Tail).
-
 write_str([]):-!.
-write_str([H|Tail]):- put(H), write_str(Tail).
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+read_list_str(List):-read_str(A,_,Flag),read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+	read_str(A,_,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
 
 % 1_1
-upr1_1:- tell('c:/Users/Neutrino/Desktop/p1_out.txt'), build_all_razm_p, told.
-build_all_razm_p:- read_str(A,_), read(K), b_a_rp(A,K,[]).
+upr1_1:- see('c:/Users/Neutrino/Desktop/p1_in.txt'), read_list_str(_, LengthList),
+	seen, max(LengthList, Max), write(Max).
 
-b_a_rp(_,0,Perm1):- write_str(Perm1), nl, !, fail.
-b_a_rp(A,N,Perm):- in_list(A,El), N1 is N-1, b_a_rp(A,N1,[El|Perm]).
+read_list_str(List, LengthList):-read_str(A,N,Flag),read_list_str([A],List,[N],LengthList,Flag).
+read_list_str(List,List,LengthList, LengthList,1):-!.
+read_list_str(Cur_list,List,CurLengthList,LengthList,0):-
+	read_str(A,N,Flag),append(Cur_list,[A],C_l),append(CurLengthList, [N], NewLengthList),read_list_str(C_l,List,NewLengthList,LengthList,Flag).
+
+max(List, MaxEl):- max(List, 0, MaxEl).
+max([],CurMax, CurMax):- !.
+max([H|T], CurMax, X):- H > CurMax, NewMax is H, max(T,NewMax,X), !.
+max([_|T], CurMax, X):- max(T, CurMax, X).
+
+% 1_2
+upr1_2:- see('c:/Users/Neutrino/Desktop/p1_in.txt'), read_list_str_space(_, C),
+	seen, write("C = "), write(C).
+
+read_str_symbol(A,Count,S,Flag):- get0(X), r_str_symbol(X,A,[],Count,0,S,Flag).
+r_str_symbol(-1,A,A,Count,Count,_,1):-!.
+r_str_symbol(10,A,A,Count,Count,_,0):-!.
+r_str_symbol(X,A,B,Count,C,S,Flag):- append(B,[X],B1), get0(X1),
+	(X = S -> C1 is C+1; C1 is C), r_str_symbol(X1,A,B1,Count,C1,S,Flag).
+
+read_list_str_space(List, C):- read_str_symbol(A,Count,32,Flag),
+	(Count = 0 -> C1 = 1; C1 = 0), read_list_str_space([A],List,C1,C,Flag).
+
+read_list_str_space(List,List,C,C,1):-!.
+
+read_list_str_space(Cur_list,List,CurC,C,0):-
+	read_str_symbol(A,Count,32,Flag), append(Cur_list,[A],C_l),
+	(Count = 0 -> C1 is CurC+1; C1 is CurC),
+	read_list_str_space(C_l,List,C1,C,Flag).
+
+% 1_3
+upr1_3:- see('c:/Users/Neutrino/Desktop/p1_in.txt'), count_A(_,N,C), M is C/N,
+	 seen, see('c:/Users/Neutrino/Desktop/p1_in.txt'), read_list_str_A(_,M), seen.
+
+count_A(List,N,C):- read_str_symbol(_,Count,65,Flag),
+	N1 = 1, count_A(List,N1,N,Count,C,Flag).
+count_A(_,N,N,C,C,1):-!.
+count_A(List,CurN,N,CurC,C,0):- read_str_symbol(_,Count,65,Flag),
+	C1 is CurC + Count, N1 is CurN + 1,
+	count_A(List,N1,N,C1,C,Flag).
+
+read_list_str_A(List, M):- read_str_symbol(A,Count,65,Flag),
+	(Count > M -> write_str(A), nl; true), read_list_str_A(List,M,Flag).
+
+read_list_str_A(_,_,1):-!.
+
+read_list_str_A(List,M,0):-
+	read_str_symbol(A,Count,65,Flag), (Count > M -> write_str(A),nl; true),
+	read_list_str_A(List,M,Flag).
+
+% 1_4
+upr1_4:- see('c:/Users/Neutrino/Desktop/p1_in.txt'), read_list_str1(List), seen,
+	freq_word(List,Freq_word), (Freq_word\=[] -> write("Word = "), write_str(Freq_word); false).
+
+read_list_str1(List):- read_str(A,_,Flag), append(A,[32],L),
+	read_list_str1(L,List,Flag).
+read_list_str1(List,List,1):-!.
+read_list_str1(Cur_list,List,0):-
+	read_str(A,_,Flag), append(Cur_list,A,C_l), append(C_l,[32],L),
+	read_list_str1(L,List,Flag).
+
+in_list([El|_],El):-!.
+in_list([_|T],El):-in_list(T,El).
+
+.
+
+get_word([],[]):- !.
+get_word([32|_],[]):- !.
+get_word([H|T1],[H|T2]):- H\=32, get_word(T1,T2),!.
+
+
+
+num_word([],_,I,I):-!.
+num_word([32|T],Word,I,C):- delete_space(T,List),
+	num_word(List,Word,I,C), !.
+num_word(List,Word,I,C):- get_word(List,W), delete_fword(List,L),
+	(W = Word -> I1 is I+1; I1 is I), num_word(L,Word,I1,C).
+
+append_list([],List2,List2).
+append_list([H|T1],List2,[H|T2]):- append_list(T1,List2,T2).
+
+delete_space([32|T],List):- delete_space(T,List),!.
+delete_space(List,List).
+
+
+delete_fword([],[]):-!.
+delete_fword([H|T1],List):- H\=32, delete_fword(T1,List),!.
+delete_fword([32|T],T):-!.
+
+
+
+freq_word(List,Freq_word):- delete_space(List,L), append_list(L,[32],Source_list),
+	freq_word(Source_list,Source_list,[],0,Freq_word).
+freq_word(_,[],Word,_,Word):-!.
+freq_word(Source_list,Changing_list,Current_word,Num,Freq_word):-
+	get_word(Changing_list,Cur), num_word(Source_list,Cur,0,I),
+	delete_fword(Changing_list, List), delete_space(List,Changed_list),
+	(I > Num -> freq_word(Source_list,Changed_list,Cur,I,Freq_word);
+	freq_word(Source_list,Changed_list,Current_word,Num,Freq_word)).
